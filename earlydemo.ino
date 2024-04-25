b6a422fa195678ee8b892c0a68dec21b02a1aaa9
gabrielwu1203 <168112733+gabrielwu1203@users.noreply.github.com>
2024-04-26 01:16:24 +0800

    Update earlydemo.ino

--- earlydemo.ino
+++ earlydemo.ino
@@ -107,10 +107,6 @@ void loop() {
         digitalWrite(pinR_DIR, 1);  
       }
     if ( !leftSensor && !rightSensor && countTjunction == 1) {
-        analogWrite(pinL_PWM, 150);
-        analogWrite(pinR_PWM, 150);
-        digitalWrite(pinL_DIR, 1);
-        digitalWrite(pinR_DIR, 1); 
         countTjunction += 1; //add 1 to countTjunction when both left and right sensors detect white
       }
   }
@@ -123,7 +119,7 @@ void loop() {
     if ( !leftSensor && !rightSensor ) {
         countTjunction += 1; //add 1 to countTjunction when both left and right sensors detect white
       }
-  else if (bumperSensor && countBumper == 1 && countTfunction == 7) // third T (stop then resume) (incomplete
+  else if (bumperSensor && countBumper == 1 && countTfunction == 7) // third T (go straight line) 
   {
     if ( !leftSensor && rightSensor ) {
         analogWrite(pinL_PWM, 150);
